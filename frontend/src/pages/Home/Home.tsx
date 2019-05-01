2baa33d8455380b0cdaa0416cade667ab3dfe36b
Antoine <antoine.eudes@gmail.com>
2019-05-01 15:27:10 +0200

    added pokemon Carapuce

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -5,15 +5,23 @@ import Style from './Home.style';
 class Home extends React.Component {
   render(): React.ReactNode {
     const pokemon = 'Carapuce';
+    const pokemonId = 7;
+    const pokeType = 'Eau';
 
     return (
       <Style.Intro>
-        <div>Bienvenue sur ton futur pokédex !</div>
         <div>
-          Tu vas pouvoir apprendre tout ce qu'il faut sur React, Redux et Symfony, et attraper des
-          pokemons !
+          <h1>
+            {pokemon} - N°{('00' + pokemonId).slice(-3)}
+          </h1>
         </div>
-        <div>Commence par créer ton premier pokemon: {pokemon}</div>
+        <div>
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`}
+            alt={pokemon}
+          />
+        </div>
+        <div>Type {pokeType}</div>
       </Style.Intro>
     );
   }
