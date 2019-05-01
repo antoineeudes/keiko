16d59920cfbb72657a73738c51db7021f70f41b1
Antoine <antoine.eudes@gmail.com>
2019-05-02 00:26:45 +0200

    when the database is empty, the 9 first pokemons are automatically added

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -6,6 +6,18 @@ import Style from './Home.style';
 
 interface Props {}
 
+const listOfPokemonsToShow = [
+  { id: 1, name: 'Bulbizarre', height: 3, weight: 4 },
+  { id: 2, name: 'Herbizarre', height: 3, weight: 4 },
+  { id: 3, name: 'Florizarre', height: 3, weight: 4 },
+  { id: 4, name: 'Salamèche', height: 3, weight: 4 },
+  { id: 5, name: 'Reptincel', height: 3, weight: 4 },
+  { id: 6, name: 'Dracaufeu', height: 3, weight: 4 },
+  { id: 7, name: 'Carapuce', height: 3, weight: 4 },
+  { id: 8, name: 'Carabaffe', height: 3, weight: 4 },
+  { id: 9, name: 'Tortank', height: 3, weight: 4 },
+];
+
 interface PokemonCaracteristics {
   id: number;
   name: string;
@@ -57,23 +69,21 @@ class Home extends React.Component<Props, State> {
         this.setState({ pokemons: response.body });
       })
       .catch(() => {
-        const data = {
-          name: 'Bulbizarre',
-          height: 1,
-          weight: 2,
-        };
-        makePostRequest('/pokemon', data)
-          .then(() => makeGetRequest('/pokemon'))
-          .then(response => this.setState({ pokemons: response.body }));
+        listOfPokemonsToShow.map(pokemon => {
+          var data = {
+            name: pokemon.name,
+            height: pokemon.height,
+            weight: pokemon.weight,
+          };
+          makePostRequest('/pokemon', data);
+        });
+        this.setState({ pokemons: listOfPokemonsToShow });
       });
   }
 
   render(): React.ReactNode {
     return (
       <Style.Intro>
-        <Pokemon name="Carapuce" id={7} />
-        <Pokemon name="Carabaffe" id={8} />
-        <Pokemon name="Tortank" id={9} />
         {this.state.pokemons.length == 0 ? (
           <h1>Loading...</h1>
         ) : (
