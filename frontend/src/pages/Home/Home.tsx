93ccdc2b762e198f88dfcdafdfd51fdab41e71bc
Antoine <antoine.eudes@gmail.com>
2019-05-02 17:33:33 +0200

    Update Home.tsx

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,21 +1,9 @@
 import * as React from 'react';
 import Pokemon from 'components/Pokemon';
-import { makeGetRequest, makePostRequest } from '../../services/networking/request';
+import { makeGetRequest } from '../../services/networking/request';
 import Style from './Home.style';
 
 interface Props {}
-
-const listOfPokemonsToShow = [
-  { id: 1, name: 'Bulbizarre', height: 3, weight: 4 },
-  { id: 2, name: 'Herbizarre', height: 3, weight: 4 },
-  { id: 3, name: 'Florizarre', height: 3, weight: 4 },
-  { id: 4, name: 'Salamèche', height: 3, weight: 4 },
-  { id: 5, name: 'Reptincel', height: 3, weight: 4 },
-  { id: 6, name: 'Dracaufeu', height: 3, weight: 4 },
-  { id: 7, name: 'Carapuce', height: 3, weight: 4 },
-  { id: 8, name: 'Carabaffe', height: 3, weight: 4 },
-  { id: 9, name: 'Tortank', height: 3, weight: 4 },
-];
 
 interface PokemonCaracteristics {
   id: number;
@@ -60,24 +48,7 @@ class Home extends React.Component<Props, State> {
   }
 
   componentDidMount() {
-    makeGetRequest('/pokemon')
-      .then(response => {
-        if (response.body.length === 0) {
-          throw 'datablase empty';
-        }
-        this.setState({ pokemons: response.body });
-      })
-      .catch(() => {
-        listOfPokemonsToShow.map(pokemon => {
-          var data = {
-            name: pokemon.name,
-            height: pokemon.height,
-            weight: pokemon.weight,
-          };
-          makePostRequest('/pokemon', data);
-        });
-        this.setState({ pokemons: listOfPokemonsToShow });
-      });
+    makeGetRequest('/pokemon').then(response => this.setState({ pokemons: response.body }));
   }
 
   render(): React.ReactNode {
