fb4ae788b0cb41de8e4070865337765c484f01ed
Antoine <antoine.eudes@gmail.com>
2019-06-04 14:01:16 +0200

    Update Home.tsx

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Pokemon from 'components/Pokemon';
-import { makeGetRequest, makePostRequest } from '../../services/networking/request';
-
+import { makeGetRequest } from '../../services/networking/request';
 import Style from './Home.style';
 
 interface Props {}
@@ -16,30 +15,6 @@ interface State {
   pokemons: PokemonCaracteristics[];
 }
 
-interface ListItemProps {
-  key: number;
-  value: PokemonCaracteristics;
-}
-
-function ListItem(props: ListItemProps) {
-  return (
-    <li>
-      <Pokemon name={props.value.name} id={props.value.id} />
-    </li>
-  );
-}
-
-function ListOfPokemons(props: State) {
-  const PokemonList = props.pokemons;
-  return (
-    <ul>
-      {PokemonList.map(pokemon => (
-        <ListItem key={pokemon.id} value={pokemon} />
-      ))}
-    </ul>
-  );
-}
-
 class Home extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
@@ -49,35 +24,22 @@ class Home extends React.Component<Props, State> {
   }
 
   componentDidMount() {
-    makeGetRequest('/pokemon')
-      .then(response => {
-        if (response.body.length === 0) {
-          throw 'datablase empty';
-        }
-        this.setState({ pokemons: response.body });
-      })
-      .catch(() => {
-        const data = {
-          name: 'Bulbizarre',
-          height: 1,
-          weight: 2,
-        };
-        makePostRequest('/pokemon', data)
-          .then(() => makeGetRequest('/pokemon'))
-          .then(response => this.setState({ pokemons: response.body }));
-      });
+    makeGetRequest('/pokemon').then(response => this.setState({ pokemons: response.body }));
   }
 
   render(): React.ReactNode {
     return (
       <Style.Intro>
-        <Pokemon name="Carapuce" id={7} />
-        <Pokemon name="Carabaffe" id={8} />
-        <Pokemon name="Tortank" id={9} />
         {this.state.pokemons.length == 0 ? (
           <h1>Loading...</h1>
         ) : (
-          <ListOfPokemons pokemons={this.state.pokemons} />
+          <ul>
+            {this.state.pokemons.map(pokemon => (
+              <li>
+                <Pokemon name={pokemon.name} id={pokemon.id} />
+              </li>
+            ))}
+          </ul>
         )}
       </Style.Intro>
     );
