c065372ae07f9322745517ac43e6f1cfdead695b
Antoine <antoine.eudes@gmail.com>
2019-06-04 14:55:02 +0200

    added pokemon font

---
+++ frontend/src/components/Pokemon/Pokemon.style.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export default {
+  Card: styled.div`
+    font-size: 20px;
+    font-family: 'PokemonGB';
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding: 10px;
+  `,
+};
--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Style from './Pokemon.style';
 
 interface Props {
   name: string;
@@ -8,21 +9,17 @@ interface Props {
 class Pokemon extends React.Component<Props> {
   render() {
     return (
-      <div>
-        <div>
-          <h1>
-            {this.props.name} - N°{('00' + this.props.id).slice(-3)}
-          </h1>
-        </div>
-        <div>
-          <img
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-              this.props.id
-            }.png`}
-            alt={this.props.name}
-          />
-        </div>
-      </div>
+      <Style.Card>
+        <h1>
+          {this.props.name} - N°{this.props.id}
+        </h1>
+        <img
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+            this.props.id
+          }.png`}
+          alt={this.props.name}
+        />
+      </Style.Card>
     );
   }
 }
