ed5fe554901f860a38bf65caf96a45b20747835b
Antoine <antoine.eudes@gmail.com>
2019-06-07 13:57:36 +0200

    added hook useState

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Style from './Pokemon.style';
 import turnIco from '../../turn-ico.svg';
 
@@ -9,21 +9,24 @@ interface Props {
   height: number;
 }
 
-const Pokemon = (props: Props) => (
-  <Style.Card>
-    <Style.TurnIco src={turnIco} alt="turn-ico" />
-    <Style.Header>{props.name}</Style.Header>
-    <img
-      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-        props.id
-      }.png`}
-      alt={props.name}
-    />
-    <p>
-      Id: {props.id} <br />
-      Weight: {props.weight / 10} kg <br />
-      Height: {props.height * 10} cm
-    </p>
-  </Style.Card>
-);
+function Pokemon(props: Props) {
+  const [turned, setTurned] = useState(false);
+  const urlPrefix = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon';
+  return (
+    <Style.Card>
+      <Style.TurnIco src={turnIco} alt="turn-ico" onClick={() => setTurned(!turned)} />
+      <Style.Header>{props.name}</Style.Header>
+      {turned ? (
+        <img src={`${urlPrefix}/back/${props.id}.png`} alt={props.name} />
+      ) : (
+        <img src={`${urlPrefix}/${props.id}.png`} alt={props.name} />
+      )}
+      <p>
+        Id: {props.id} <br />
+        Weight: {props.weight / 10} kg <br />
+        Height: {props.height * 10} cm
+      </p>
+    </Style.Card>
+  );
+}
 export default Pokemon;
