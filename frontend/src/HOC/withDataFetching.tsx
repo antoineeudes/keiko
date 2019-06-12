73d5e0d15849714ca5b87001974d9adc5ad7a1f6
Antoine <antoine.eudes@gmail.com>
2019-06-12 13:23:45 +0200

    not working

---
+++ frontend/src/HOC/withDataFetching.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Loader from 'components/Loader';
+
+interface WithDataFetchingProps {
+  loading: boolean;
+  error: boolean;
+}
+
+const WithDataFetching = <Props extends object>(
+  dataName: string,
+  fetchFunction: (props: Props) => any,
+  shouldCallEffect: (props: Props) => any[],
+) => (BaseComponent: React.ComponentType<Props>) => (props: Props) => {
+  let loading = true;
+  let error = false;
+  try {
+    const response = fetchFunction(props);
+    console.log(response);
+    loading = false;
+  } catch {
+    loading = false;
+    error = true;
+  }
+  console.log(error);
+  return (
+    (loading && <Loader />) ||
+    (error && <p>Une erreur est survenue</p>) || <BaseComponent {...props} />
+  );
+};
+
+export default WithDataFetching;
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import Pokemon from 'components/Pokemon';
+// import Pokemon from 'components/Pokemon';
 import { RouteComponentProps } from 'react-router-dom';
 import { makeGetRequest } from 'services/networking/request';
-import { PageContainer, Header, Link, PokemonList } from './Home.style';
-import Loader from 'components/Loader';
+// import { PageContainer, Header, Link, PokemonList } from './Home.style';
+import HOC from 'HOC';
 
 export interface PokemonCaracteristics {
   id: number;
@@ -14,69 +14,33 @@ export interface PokemonCaracteristics {
 
 type urlParams = { page: string };
 
+interface HomeProps {
+  pokemons: PokemonCaracteristics[];
+  match: RouteComponentProps<urlParams>;
+}
+
 function Home({ match }: RouteComponentProps<urlParams>) {
-  const [pokemons, setPokemons] = useState<PokemonCaracteristics[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+  return <h1>Component Rendered</h1>;
+}
+
+// async function fetchPokemons(props: HomeProps) {
+//   let page = 1;
+//   if (props.match.params.page != undefined) {
+//     page = Number(props.match.params.page);
+//   }
+//   return makeGetRequest(`/pokemon?page=${page}`);
+// }
+
+async function fetchPokemons({ match }: RouteComponentProps<urlParams>) {
   let page = 1;
   if (match.params.page != undefined) {
     page = Number(match.params.page);
   }
-
-  async function fetchPokemons() {
-    try {
-      setLoading(true);
-      const response = await makeGetRequest(`/pokemon?page=${page}`);
-      setPokemons(response.body);
-      setLoading(false);
-    } catch {
-      setError(true);
-      setLoading(false);
-    }
-  }
-
-  useEffect(
-    () => {
-      fetchPokemons();
-    },
-    [match.params.page],
-  );
-
-  return (
-    <PageContainer>
-      <Header>
-        {!error && !loading && page > 1 ? (
-          <Link to={`/pokedex/${page - 1}`}>&lsaquo;</Link>
-        ) : (
-          <div />
-        )}
-        <h1>Pokedex</h1>
-        {!error && !loading && page < 6 ? (
-          <Link to={`/pokedex/${page + 1}`}>&rsaquo;</Link>
-        ) : (
-          <div />
-        )}
-      </Header>
-      {error ? (
-        <p>Une erreur est survenue</p>
-      ) : loading ? (
-        <Loader />
-      ) : (
-        <PokemonList>
-          {pokemons.map(pokemon => (
-            <Link to={`/pokemon/${pokemon.id}`} key={pokemon.id}>
-              <Pokemon
-                name={pokemon.name}
-                id={pokemon.id}
-                weight={pokemon.weight}
-                height={pokemon.height}
-              />
-            </Link>
-          ))}
-        </PokemonList>
-      )}
-    </PageContainer>
-  );
+  return makeGetRequest(`/pokemon?page=${page}`);
 }
 
-export default Home;
+const oneShouldCallEffect = (props: HomeProps) => [undefined];
+
+const HomeWrap = HOC(fetchPokemons, oneShouldCallEffect)(Home);
+
+export default HomeWrap;
